e6738eb5fe48978d9052b9d00ebcf30891494adf
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-07-26 16:11:12 +0530

    Create lakshmi.java

---
+++ lakshmi.java
@@ -0,0 +1,17 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=90;
+		if(n>0)
+		{
+			System.out.println("n is positive num");
+		}
+		else if(n<0)
+		{
+			System.out.println("n is negetive num");
+			} 
+		else
+		System.out.println(" n is zero");
+		}
+}
